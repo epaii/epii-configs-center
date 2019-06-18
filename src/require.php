fc926be9f6347cbe9a4b37a7c8de52b1afc87173
songyang <475511896@qq.com>
2019-06-18 16:32:09 +0800

    songyang commit

---
+++ src/require.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019-06-18
+ * Time: 16:30
+ */
+
+require_once __DIR__."/ConfigManager.php";
+require_once __DIR__."/ConfigsCenter.php";
+require_once __DIR__."/ConfigTools.php";
